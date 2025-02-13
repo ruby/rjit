7a5de7b573202623eaba8215b3f6725320e10285
Takashi Kokubun <takashikkbn@gmail.com>
2025-02-12 20:20:41 -0800

    Rename Rjit to RJIT

--- lib/rjit.rb
+++ lib/rjit.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "rjit/version"
+require_relative 'rjit/version'
 
-module Rjit
-  class Error < StandardError; end
-  # Your code goes here...
+module RJIT
 end
--- lib/rjit/version.rb
+++ lib/rjit/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-module Rjit
-  VERSION = "0.1.0"
+module RJIT
+  VERSION = '0.1.0'
 end
