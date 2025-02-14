b81ba6928cf5cec001dc2d4a4e7896cc3f27e3d8
Takashi Kokubun <takashikkbn@gmail.com>
2025-02-13 22:51:03 -0800

    Update bindgen for ruby/rjit

--- lib/rjit/version.rb
+++ lib/rjit/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 module RJIT
-  VERSION = '0.1.0'
+  # I'm planning on releasing "3.5.0" once Ruby 3.5.0 is released,
+  # which is "RUBY_VERSION_MAJOR.RUBY_VERSION_MINOR.RJIT_PATCH_VERSION".
+  VERSION = '0.0.1'
 end
